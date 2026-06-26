a8bccdb2068c4a908c0ff21076f662918f84a038
James Enmanuel Urena Paulino <james_urena@ucne.edu.do>
2026-06-25 21:48:16 -0400

    Arregla estructura igual al documento del GetPlanetsUseCase.kt y el GetPlanetDetailUseCase.kt

--- app/src/main/java/edu/ucne/dragonball_planets/domain/usecase/GetPlanetsUseCase.kt
+++ app/src/main/java/edu/ucne/dragonball_planets/domain/usecase/GetPlanetsUseCase.kt
@@ -1,18 +1,19 @@
 package edu.ucne.dragonball_planets.domain.usecase
 
 import edu.ucne.dragonball_planets.data.remote.Resource
-import edu.ucne.dragonball_planets.data.remote.dto.PlanetDto
+import edu.ucne.dragonball_planets.domain.model.Planet
 import edu.ucne.dragonball_planets.domain.repository.PlanetRepository
+import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
 
 class GetPlanetsUseCase @Inject constructor(
     private val repository: PlanetRepository
 ) {
-    suspend operator fun invoke(
+    operator fun invoke(
         page: Int = 1,
         limit: Int = 10,
         name: String? = null
-    ): Resource<List<PlanetDto>> {
-        return repository.getPlanets(page, limit, name, null)
+    ): Flow<Resource<List<Planet>>> {
+        return repository.getPlanets(page, limit, name)
     }
 }
