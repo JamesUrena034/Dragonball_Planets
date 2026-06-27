25c744bc1d8025529bcb64cc49d2fff33f24d15a
James Enmanuel Urena Paulino <james_urena@ucne.edu.do>
2026-06-27 01:57:52 -0400

    Modifica el MainActivity.kt

--- app/src/main/java/edu/ucne/dragonball_planets/MainActivity.kt
+++ app/src/main/java/edu/ucne/dragonball_planets/MainActivity.kt
@@ -4,10 +4,13 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.material3.DrawerValue
+import androidx.compose.material3.rememberDrawerState
 import androidx.navigation.compose.rememberNavController
 import dagger.hilt.android.AndroidEntryPoint
-import edu.ucne.dragonball_planets.presentation.navigation.NavHost
 import edu.ucne.dragonball_planets.ui.theme.Dragonball_PlanetsTheme
+import edu.ucne.dragonball_planets.presentation.navigation.NavHost
+import edu.ucne.planetapi.presentation.navigation.DrawerMenu
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
@@ -17,7 +20,17 @@ class MainActivity : ComponentActivity() {
         setContent {
             Dragonball_PlanetsTheme {
                 val navController = rememberNavController()
-                NavHost(navHostController = navController)
+                val drawerState = rememberDrawerState(initialValue = DrawerValue.Closed)
+
+                DrawerMenu(
+                    drawerState = drawerState,
+                    navHostController = navController
+                ) {
+                    NavHost(
+                        navHostController = navController,
+                        drawerState = drawerState
+                    )
+                }
             }
         }
     }
